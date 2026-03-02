1f082e6363f0fcbb4757de34804dad1d48b7955f
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2026-03-02 17:55:18 -0500

    Create vehicles.ts

---
+++ data/vehicles.ts
@@ -0,0 +1,37 @@
+import { Vehicle } from "@/types/vehicle";
+
+export const vehicles: Vehicle[] = [
+  {
+    id: "rav4",
+    make: "Toyota",
+    model: "RAV4",
+    year: 2024,
+    mpg: "27 city / 35 highway",
+    cargo: "69.8 cu ft",
+    safety: "5-Star NHTSA",
+    horsepower: "203 hp",
+    price: "$28,675"
+  },
+  {
+    id: "crv",
+    make: "Honda",
+    model: "CR-V",
+    year: 2024,
+    mpg: "28 city / 34 highway",
+    cargo: "76.5 cu ft",
+    safety: "5-Star NHTSA",
+    horsepower: "190 hp",
+    price: "$29,500"
+  },
+  {
+    id: "cx5",
+    make: "Mazda",
+    model: "CX-5",
+    year: 2024,
+    mpg: "26 city / 31 highway",
+    cargo: "59.3 cu ft",
+    safety: "5-Star NHTSA",
+    horsepower: "187 hp",
+    price: "$28,570"
+  }
+];
