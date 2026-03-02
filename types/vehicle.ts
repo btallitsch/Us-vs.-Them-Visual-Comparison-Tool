43698d051c2ed95c5feac62fcb1f790abaf8ba5b
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2026-03-02 17:57:23 -0500

    Update vehicle.ts

--- types/vehicle.ts
+++ types/vehicle.ts
@@ -3,9 +3,11 @@ export interface Vehicle {
   make: string;
   model: string;
   year: number;
-  mpg: string;
-  cargo: string;
-  safety: string;
-  horsepower: string;
-  price: string;
+
+  price: number;        // lower is better
+  cityMpg: number;      // higher is better
+  highwayMpg: number;   // higher is better
+  cargo: number;        // cubic feet, higher better
+  horsepower: number;   // higher better
+  safetyRating: number; // 1–5 scale, higher better
 }
