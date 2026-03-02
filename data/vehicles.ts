3fbb7c043c6e57231a0b031b239030d4a8ef3169
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2026-03-02 17:57:37 -0500

    Update vehicles.ts

--- data/vehicles.ts
+++ data/vehicles.ts
@@ -1,37 +1,26 @@
-import { Vehicle } from "@/types/vehicle";
-
 export const vehicles: Vehicle[] = [
   {
     id: "rav4",
     make: "Toyota",
     model: "RAV4",
     year: 2024,
-    mpg: "27 city / 35 highway",
-    cargo: "69.8 cu ft",
-    safety: "5-Star NHTSA",
-    horsepower: "203 hp",
-    price: "$28,675"
+    price: 28675,
+    cityMpg: 27,
+    highwayMpg: 35,
+    cargo: 69.8,
+    horsepower: 203,
+    safetyRating: 5
   },
   {
     id: "crv",
     make: "Honda",
     model: "CR-V",
     year: 2024,
-    mpg: "28 city / 34 highway",
-    cargo: "76.5 cu ft",
-    safety: "5-Star NHTSA",
-    horsepower: "190 hp",
-    price: "$29,500"
-  },
-  {
-    id: "cx5",
-    make: "Mazda",
-    model: "CX-5",
-    year: 2024,
-    mpg: "26 city / 31 highway",
-    cargo: "59.3 cu ft",
-    safety: "5-Star NHTSA",
-    horsepower: "187 hp",
-    price: "$28,570"
+    price: 29500,
+    cityMpg: 28,
+    highwayMpg: 34,
+    cargo: 76.5,
+    horsepower: 190,
+    safetyRating: 5
   }
 ];
