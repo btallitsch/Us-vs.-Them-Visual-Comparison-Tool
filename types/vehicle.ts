bdad103029aa4b08f594cd6dfa5ac47e66bfe525
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2026-03-02 17:55:01 -0500

    Create vehicle.ts

---
+++ types/vehicle.ts
@@ -0,0 +1,11 @@
+export interface Vehicle {
+  id: string;
+  make: string;
+  model: string;
+  year: number;
+  mpg: string;
+  cargo: string;
+  safety: string;
+  horsepower: string;
+  price: string;
+}
